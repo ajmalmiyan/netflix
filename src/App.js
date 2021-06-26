6d795a5c734cfe5c22bf3d6fe83b0dd2b5686a08
Ajmal KV <reallyaju@gmail.com>
2021-06-26 15:18:52 +0530

    fixed jumbo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import Jumbotron from './components/Jumbotron/'
-import jumboData from './fixtures/jumbo.json'
+import JumbotronContainer from './Containers/Jumbotron';
 
 export default function App() {
   return (
-    <Jumbotron.Container>
-      {
-        jumboData.map(item=>(
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTtile>{item.subTitle}</Jumbotron.SubTtile>
-            </Jumbotron.Pane>
-            <Jumbotron.Pane>
-              <Jumbotron.Image src={item.image} alt={item.alt}/>
-            </Jumbotron.Pane>
-          </Jumbotron>
-        ))
-      }
-    </Jumbotron.Container>
+    <JumbotronContainer/>
   );
 }
