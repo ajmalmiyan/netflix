3b74edc95ae33b20dcc2ed4ff7300745db4f9875
Ajmal KV <reallyaju@gmail.com>
2021-06-26 18:32:37 +0530

    footer link fixture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import FooterContainer from './Containers/Footer';
 import JumbotronContainer from './Containers/Jumbotron';
 
 export default function App() {
   return (
+    <>
     <JumbotronContainer/>
+    <FooterContainer/>
+    </>
   );
 }
